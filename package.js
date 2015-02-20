2664a39316698856a31762bbfc7e6936e9349521
Pusher Robot <jack@semantic-ui.com>
2015-02-20 17:15:38 -0500

    Updated component to version 1.9.3

--- package.js
+++ package.js
@@ -1,17 +1,14 @@
-var
-  where = 'client' // Adds files only to the client
-;
 
 Package.describe({
   name    : 'semantic:ui-feed',
-  summary : 'Semantic UI - Feed (official): Single component release of feed',
-  version : '1.9.2',
+  summary : 'Semantic UI - Feed: Single component release',
+  version : '1.9.3',
   git     : 'git://github.com/Semantic-Org/UI-Feed.git',
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.0');
   api.addFiles([
-    
-  ], where);
+    'feed.css'
+  ], 'client');
 });
