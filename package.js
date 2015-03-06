710165a2b8f6fa7dc5c59addf0e1a01ca05ad311
Pusher Robot <jack@semantic-ui.com>
2015-03-06 18:21:51 -0500

    Updated component to version 1.11.2

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 Package.describe({
   name    : 'semantic:ui-feed',
   summary : 'Semantic UI - Feed: Single component release',
-  version : '1.11.1',
+  version : '1.11.2',
   git     : 'git://github.com/Semantic-Org/UI-Feed.git',
 });
 
