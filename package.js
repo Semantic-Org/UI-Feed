e5d655e07d098cb9399c7fef0b39daaf1fd20a7d
Jack Lukic <jack@semantic-ui.com>
2017-03-29 06:41:13 -0400

    Updated component to version 2.2.10

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 Package.describe({
   name    : 'semantic:ui-feed',
   summary : 'Semantic UI - Feed: Single component release',
-  version : '2.2.8',
+  version : '2.2.10',
   git     : 'git://github.com/Semantic-Org/UI-Feed.git',
 });
 
